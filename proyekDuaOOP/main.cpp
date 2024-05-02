829962c1f6b32024ee2b15b211077dff74dbeaae
Benson607 <87267159+Benson607@users.noreply.github.com>
2024-05-02 12:12:10 +0800

    增加變色

--- proyekDuaOOP/main.cpp
+++ proyekDuaOOP/main.cpp
@@ -6,8 +6,12 @@ int main() {
 	Role player1(1);
 	Role player2(2);
 	Role player3(3);
+	Enemy enemy;
 	Map map;
 	map.setRect(player1.rect);
+	map.setRect(player2.rect);
+	map.setRect(player3.rect);
+	map.setRect(enemy.rect);
 	map.setWall();
 	map.show();
 }
--- proyekDuaOOP/map.h
+++ proyekDuaOOP/map.h
@@ -3,6 +3,7 @@
 #include "rect.h"
 #include <vector>
 #include <iostream>
+#include <Windows.h>
 
 class Map:public std::vector<std::vector<Rect>> {
 public:
