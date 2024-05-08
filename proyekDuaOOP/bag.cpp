85dffbddb1f178e12780a5f5c00845b9ee0308e5
Benson607 <87267159+Benson607@users.noreply.github.com>
2024-05-08 14:57:45 +0800

    Delete proyekDuaOOP directory

--- proyekDuaOOP/bag.cpp
+++
@@ -1,8 +0,0 @@
-#include "bag.h"
-
-std::vector<Equipment> Bag::equipments = std::vector<Equipment>(0);
-std::vector<Item> Bag::items = std::vector<Item>(0);
-
-Bag::Bag() {
-
-}
--- proyekDuaOOP/role.cpp
+++
@@ -1,5 +0,0 @@
-#include "role.h"
-
-Role::Role(int num, std::string name) :Entity(num, name) {
-
-}
